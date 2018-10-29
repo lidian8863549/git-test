d2964b32b890986fd232b49e73371ecacca9019c
lidian <377548371@qq.com>
2018-10-29 18:58:54 +0800

    提交到本地

--- git-project/src/com/tuniu/test/HelloGit.java
+++ git-project/src/com/tuniu/test/HelloGit.java
@@ -4,12 +4,17 @@ public class HelloGit {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
 
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
     }
 }
