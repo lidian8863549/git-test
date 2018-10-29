70023b4ab8ecee3c5080a4941558de163d39b2bf
lidian <377548371@qq.com>
2018-10-29 19:19:22 +0800

    本次的修改

--- git-project/src/com/tuniu/test/HelloGit.java
+++ git-project/src/com/tuniu/test/HelloGit.java
@@ -10,11 +10,12 @@ public class HelloGit {
         System.out.println("hello git project!");
         System.out.println("hello git project!");
         System.out.println("hello git project!");
-
+        //hello test
     }
 
     public void test() {
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
